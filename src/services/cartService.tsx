b4579b9e30efd9a3a718125a085956cd95880614
javid shabin <javid.main.act@gmail.com>
2025-02-14 20:08:44 +0530

    Create the get all cart fucntin in cartService file

--- src/services/cartService.tsx
+++ src/services/cartService.tsx
@@ -2,3 +2,6 @@ import { axiosInstance } from "../config/axiosInstance";
 
 export const addToCartAPI = (payload: any) =>
   axiosInstance.post("/cart/add-cart", payload);
+
+export const getAllCart = () => 
+  axiosInstance.get("/cart/cart-items")
