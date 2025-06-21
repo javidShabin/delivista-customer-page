1adf26b6afb06a7cbca1255204ac75e580517b46
javid shabin <javid.main.act@gmail.com>
2025-06-22 01:17:01 +0530

    Create about page and import in home component, set grid and text alignemtns

--- src/app/about-page/page.tsx
+++ src/app/about-page/page.tsx
@@ -3,28 +3,43 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <section>
-      <div className="container">
-        <div>
-          <h1>
-            About <span>Us</span>
+    <section className="bg-white py-16 h-screen">
+      <div className="max-w-6xl mx-auto px-6">
+        <div className="mb-12 text-center">
+          <h1 className="text-4xl font-bold mb-4">
+            About <span className="text-orange-500">Us</span>
           </h1>
-          <p>
+          <p className="text-gray-600 max-w-2xl mx-auto">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
             maxime illo esse perferendis magnam, nostrum, hic voluptate
             accusamus voluptatem soluta deserunt ea culpa quis.
           </p>
         </div>
-        <div className="grid"></div>
-        <div>
-          <h2>
-            Why choose <span>Us</span>
-          </h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            laudantium nesciunt excepturi facere, modi earum, neque veritatis
-            aliquid reiciendis placeat doloremque vero quibusdam adipisci?
-          </p>
+
+        <div className="flex flex-col md:flex-row items-center justify-between gap-10">
+          {/* Custom grid design */}
+          <div className="grid grid-cols-3 grid-rows-2 gap-8 w-full max-w-4xl">
+            {/* Card 1 */}
+            <div className="col-span-1 row-span-1rounded-xl shadow-lg h-44 flex items-start justify-start"></div>
+            {/* Card 2 */}
+            <div className="col-span-2 row-span-1 rounded-xl shadow-lg h-44 flex items-start justify-start"></div>
+            {/* Card 3 */}
+            <div className="col-span-2 row-span-1 rounded-xl shadow-lg h-44 flex items-start justify-start"></div>
+            {/* Card 4 */}
+            <div className="rounded-xl shadow-lg h-44 flex items-start justify-start"></div>
+          </div>
+
+          {/* Why choose us */}
+          <div className="max-w-md">
+            <h2 className="text-2xl font-bold mb-4">
+              Why choose <span className="text-orange-500">Us</span>
+            </h2>
+            <p className="text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+              laudantium nesciunt excepturi facere, modi earum, neque veritatis
+              aliquid reiciendis placeat doloremque vero quibusdam adipisci?
+            </p>
+          </div>
         </div>
       </div>
     </section>
