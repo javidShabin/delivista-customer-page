53997f0864e2d85338f8b6ca3bb0f4f06c507310
javid shabin <javid.main.act@gmail.com>
2025-06-22 14:53:50 +0530

    Solve error in my .env BACKEND_PUBLIC_API_URL, my axios trying to get NEXT_PUBLIC_API_URL

--- src/config/axiosInstance.tsx
+++ src/config/axiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/app`,
+  baseURL: `${process.env.BACKEND_PUBLIC_API_URL}/app`,
   withCredentials: true,
 });
