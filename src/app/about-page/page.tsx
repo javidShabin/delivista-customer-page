3fc03b615d5799bbbf840f3de6f4b5bf2e1067b0
javid shabin <javid.main.act@gmail.com>
2025-06-22 13:04:54 +0530

    Change

--- src/app/about-page/page.tsx
+++ src/app/about-page/page.tsx
@@ -43,7 +43,7 @@ const AboutPage = () => {
             About <span className="text-orange-500">Us</span>
           </h1>
           <p className="text-gray-600 max-w-2xl mx-auto">
-            We&apos;re committed to delivering your favorite meals from top restaurants. 
+            We&apos;re  committed to delivering your favorite meals from top restaurants. 
             Explore menus, track orders, and enjoy fast delivery — all at your fingertips.
           </p>
         </div>
