56f615c986787dbbd1bbf5b092420c8f5fc18af7
javid shabin <javid.main.act@gmail.com>
2025-06-22 11:58:24 +0530

    Reduce the home page decoration imges size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,27 +10,27 @@ export default function Home() {
       >
         <img
           src="/assets/images/mint.png"
-          className="w-[50px] sm:w-[80px] absolute top-28 sm:top-20 right-16 sm:right-56 animate-slideRight"
+          className="w-[50px] sm:w-[70px] absolute top-28 sm:top-20 right-16 sm:right-56 animate-slideRight"
         />
         <img
           src="/assets/images/tomato.png"
-          className="w-[50px] sm:w-[80px] absolute bottom-44 right-28 sm:bottom-10 sm:right-80 animate-slideRight"
+          className="w-[50px] sm:w-[70px] absolute bottom-44 right-28 sm:bottom-10 sm:right-80 animate-slideRight"
         />
         <img
           src="/assets/images/mint1.png"
-          className="w-[70px] sm:w-[120px] absolute bottom-32 left-20 sm:left-80 animate-slideRight"
+          className="w-[70px] sm:w-[100px] absolute bottom-32 left-20 sm:left-80 animate-slideRight"
         />
         <img
           src="/assets/images/tomato1.png"
-          className="w-[90px] sm:w-[150px] absolute top-5 sm:top-10 left-14 sm:left-56 animate-slideRight"
+          className="w-[90px] sm:w-[120px] absolute top-5 sm:top-10 left-14 sm:left-56 animate-slideRight"
         />
         <img
           src="/assets/images/chilly.png"
-          className="w-[70px] sm:w-[110px] absolute bottom-20 sm:bottom-60 right-10 animate-slideRight"
+          className="w-[70px] sm:w-[90px] absolute bottom-20 sm:bottom-60 right-10 animate-slideRight"
         />
         <img
           src="/assets/images/chilly1.png"
-          className="w-[90px] sm:w-[150px] absolute bottom-96 sm:bottom-60 left-0 animate-slideRight"
+          className="w-[90px] sm:w-[120px] absolute bottom-96 sm:bottom-60 left-0 animate-slideRight"
         />
         <div className="container flex flex-col md:flex-row justify-between items-center rounded-lg px-4 sm:px-8 lg:px-12 w-11/12 h-[80vh] sm:h-[70vh]">
           {/* Text Section */}
