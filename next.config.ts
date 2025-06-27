07ff1f1603a28df0a90499938f56c7cf0c7236b6
javid shabin <javid.main.act@gmail.com>
2025-06-28 01:18:26 +0530

    maek change for deploying

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['media.istockphoto.com'], // Add other image domains here if needed
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
