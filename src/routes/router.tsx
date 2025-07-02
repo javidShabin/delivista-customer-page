a4136d6c71fb89b2b0b9016c96f78e83126b732f
javid shabin <javid.main.act@gmail.com>
2025-07-02 23:50:01 +0530

    Create the user layout in router '/'

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,11 @@
 import {
   createBrowserRouter,
 } from "react-router-dom";
+import UserLayout from "../layout/UserLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <UserLayout />,
   },
 ]);
