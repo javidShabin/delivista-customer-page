629d4e61edb0ad1d76a73709db9e9724e48d136a
javid shabin <javid.main.act@gmail.com>
2025-07-05 01:36:36 +0530

    set the route for auth users and add the single restaruant page only access auth users if not loggined the redirect ot login page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,6 +5,8 @@ import UserLayout from "../layout/UserLayout";
 import Home from "../pages/Home";
 import SignupForm from "../pages/SignupForm";
 import LoginForm from "../pages/LoginForm";
+import AuthUser from "./protectedRoutes/AuthUser";
+import SingleRestaurant from "../pages/SingleRestaurant";
 
 export const router = createBrowserRouter([
   {
@@ -24,7 +26,20 @@ export const router = createBrowserRouter([
         path: "login-page",
         element: <LoginForm />
       },
-      
+
+      // Loggin-in user
+      {
+        path: "user",
+        element: <AuthUser />,
+
+        children: [
+          {
+            path: "/restaurant/:id",
+            element: <SingleRestaurant />
+          }
+        ]
+      }
+
     ]
   },
 ]);
