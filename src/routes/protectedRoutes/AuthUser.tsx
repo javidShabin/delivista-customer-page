4a159eab8e71a009ce3599b213c0c80c1f893d0c
javid shabin <javid.main.act@gmail.com>
2025-07-05 01:34:20 +0530

    Write logic for unauth users redircting to login page

--- src/routes/protectedRoutes/AuthUser.tsx
+++ src/routes/protectedRoutes/AuthUser.tsx
@@ -1,10 +1,17 @@
+import { useSelector } from "react-redux";
+import type { RootState } from "../../redux/store";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const AuthUser = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { isUserExist } = useSelector((state: RootState) => state.user);
+  const navigate = useNavigate();
 
-export default AuthUser
+  // Check the user is loggined or not
+  // If not loggined redirect to login page
+  if (!isUserExist) {
+    navigate("login-page");
+  }
+  return isUserExist ? <Outlet /> : null;
+};
+
+export default AuthUser;
