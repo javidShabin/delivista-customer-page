a2de9009c24bbac846384da28cdf73b9d296e2b8
javid shabin <javid.main.act@gmail.com>
2025-07-26 03:09:11 +0530

    Create api  all in wishlist service file for all list of items, and get response

--- src/pages/authUser/Dashboard/Favoites.tsx
+++ src/pages/authUser/Dashboard/Favoites.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react"
+import { getFavorites } from "../../../services/wishlistService"
 
 const Favoites = () => {
+
+  useEffect(() => {
+    const getFavList = async () => {
+      try {
+        const resposne = await getFavorites()
+        console.log(resposne)
+      } catch (error) {
+        
+      }
+    }
+    getFavList()
+  },[])
+
   return (
     <div>Favoites</div>
   )
--- src/services/wishlistService.tsx
+++ src/services/wishlistService.tsx
@@ -3,3 +3,8 @@ import { axiosInstance } from "../config/axiosInstance";
 // Add item to wishlist
 export const addToWishlist = (menuId: string) =>
   axiosInstance.post("/wishlist/add", { menuId });
+
+// Get list from wishlist
+export const getFavorites = () => {
+  axiosInstance.get("/wishlist/get-list")
+}
