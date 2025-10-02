6c180405f6cec36267e8c93048ff1a51535ac95c
javid shabin <javid.main.act@gmail.com>
2025-10-02 14:47:19 +0530

    Make change

--- src/routes/protectedRoutes/AuthUser.tsx
+++ src/routes/protectedRoutes/AuthUser.tsx
@@ -1,16 +1,20 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../../redux/store";
 import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const AuthUser = () => {
   const { isUserExist } = useSelector((state: RootState) => state.user);
   const navigate = useNavigate();
 
-  // Check the user is loggined or not
-  // If not loggined redirect to login page
-  if (!isUserExist) {
-    navigate("/login-page");
-  }
+  useEffect(() => {
+    // Check the user is loggined or not
+    // If not loggined redirect to login page
+    if (!isUserExist) {
+      navigate("/login-page");
+    }
+  }, [isUserExist, navigate]);
+
   return isUserExist ? <Outlet /> : null;
 };
 
